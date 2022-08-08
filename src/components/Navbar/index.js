7f40b3b7b3d41fd560c19c423ade91b662850e10
killiangr <killian.graindorge@efrei.net>
2022-08-08 12:47:36 +0200

    Finish NavBar and Hero

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,18 @@
-import React from "react";
-import {Nav, NavLink} from "./NavbarElements";
+import React from 'react';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
 
-const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavLink to="/">Pizza</NavLink>
-            </Nav>
-        </>
-    );
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>Pizza</NavLink>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
